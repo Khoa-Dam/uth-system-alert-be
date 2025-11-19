e5bc5b32d2e32ff85c7e008c07aece58c8515194
Khoa Dam <damngockhoa0703@gmail.com>
2025-11-20 01:35:38 +0700

    Add entity wanted

---
+++ src/entities/wanted-criminal.entity.ts
@@ -0,0 +1,32 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+
+@Entity('wanted_criminals')
+export class WantedCriminal {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string; // Họ tên đối tượng
+
+    @Column()
+    birthYear: number; // Năm sinh
+
+    @Column({ nullable: true })
+    address: string; // Nơi ĐKTT (Đăng ký thường trú)
+
+    @Column({ nullable: true })
+    parents: string; // Họ tên bố/mẹ
+
+    @Column()
+    crime: string; // Tội danh
+
+    @Column({ nullable: true })
+    decisionNumber: string; // Số ngày QĐ
+
+    @Column({ nullable: true })
+    issuingUnit: string; // Đơn vị ra QĐTN (Quyết định truy nã)
+
+    @CreateDateColumn()
+    createdAt: Date;
+}
+
