09ef94ed6d56b7569d5458b4c1d077009c9d260f
Khoa Dam <damngockhoa0703@gmail.com>
2025-11-27 08:25:25 +0700

    fix: bug jwt weather

--- src/weather-news/weather-news.module.ts
+++ src/weather-news/weather-news.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { WeatherNews } from './entities/weather-news.entity';
 import { WeatherNewsService } from './weather-news.service';
 import { WeatherNewsController } from './weather-news.controller';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([WeatherNews])],
+    imports: [
+        TypeOrmModule.forFeature([WeatherNews, User]),
+        JwtModule.register({}),
+    ],
     providers: [WeatherNewsService],
     controllers: [WeatherNewsController],
     exports: [WeatherNewsService],
